0601f9bb180ae70192043d275c5575800624c607
Abhineet Bhattacharjee <abhineetbhattacharjee@gmail.com>
2026-02-05 01:26:49 +0530

    chore: read listing links xpath from selectors config

--- scraper.py
+++ scraper.py
@@ -236,6 +236,7 @@ class MapsLeadScraper:
 
     def get_listing_links(self) -> list[str]:
         feed = self.get_results_container()
+        links_xpath = self.selectors['listing_links']['xpath']
 
     @staticmethod
     def load_selectors() -> dict[str, str]:
