c737788944145d4b9adc4ac2324b087b79ded31a
Abhineet Bhattacharjee <abhineetbhattacharjee@gmail.com>
2026-02-13 00:37:25 +0700

    feat: extract address from link site

--- scraper.py
+++ scraper.py
@@ -328,6 +328,10 @@ class MapsLeadScraper:
             (By.XPATH, self.selectors["business_type"])
         )
 
+        address = self.safe_find_element(
+            (By.XPATH, self.selectors["business_address"])
+        )
+
     @staticmethod
     def load_selectors() -> dict[str, str]:
         with open('selectors.yaml', 'r') as file:
